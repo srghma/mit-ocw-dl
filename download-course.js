1a18918e67dfec5d2673c45bd182515c0e2c0139
Serhii Khoma <srghma@gmail.com>
2023-08-24 17:02:27 +0700

    feat: update

---
+++ download-course.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+// ./download-course.js https://ocw.mit.edu/courses/14-01sc-principles-of-microeconomics-fall-2011/ 
+
+const fs = require('fs').promises;
+const path = require('path');
+const { exec } = require('child_process');
+const { promisify } = require('util');
+// const rra = require('recursive-readdir-async')
+
+const execAsync = promisify(exec);
+
+async function findPDFsWithGraphs(rootDir) {
+    const pdfFiles = [];
+
+    async function findPDFs(dir) {
+        const files = await fs.readdir(dir);
+        const statPromises = files.map(async file => {
+            const filePath = path.join(dir, file);
+            const stat = await fs.stat(filePath);
+            if (stat.isDirectory()) {
+                await findPDFs(filePath);
+            } else if (stat.isFile() && file.endsWith('.pdf') && file.includes('graph')) {
+                pdfFiles.push(filePath);
+            }
+        });
+        await Promise.all(statPromises);
+    }
+
+    await findPDFs(rootDir);
+    return pdfFiles;
+}
+
+async function extractImages(pdfFilePath) {
+    const folderName = pdfFilePath.replace('.pdf', '');
+    const outputPath = path.join(folderName, '%d.jpg');
+
+    try {
+        await fs.mkdir(folderName);
+    } catch (err) {
+        if (err.code !== 'EEXIST') {
+            console.error('Error creating directory:', err);
+            return;
+        }
+    }
+
+    try {
+        await execAsync(`pdftoppm -jpeg "${pdfFilePath}" "${outputPath}"`);
+        // await execAsync(`pdfimages -all "${pdfFilePath}" "${outputPath}"`);
+        console.log('Images extracted:', pdfFilePath);
+    } catch (err) {
+        console.error('Error extracting images:', err);
+    }
+}
+
+async function main() {
+    if (process.argv.length !== 3) {
+        console.error('Usage: node find-pdfs-convert-images.js <rootDirectory>');
+        process.exit(1);
+    }
+
+    const rootDirectory = path.resolve(process.argv[2]);
+    const pdfFilesWithGraphs = await findPDFsWithGraphs(rootDirectory);
+
+    const extractPromises = pdfFilesWithGraphs.map(pdfFile => extractImages(pdfFile));
+    await Promise.all(extractPromises);
+}
+
+main().catch(err => console.error('Error:', err));
